56e941d5cb7397b82395533a1cb09c45327a19ef
Jhamil Taborga <jhamiltaborga@gmail.com>
2025-01-30 18:22:55 -0400

    Utilizamos react-router-dom como un react-hook para realizar el enrutamiento de las páginas

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -6,16 +7,24 @@ import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },/*El "*" hace que cuando se pone cualquier otra ruta que no tengamos definidas acá las dirige a este componente (a esa página)*/
+  ])
+
+  return routes
+}
+
+const App = () => {  
   return (
-    <div className="bg-red-600">
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
