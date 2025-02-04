973b238e18ef161c182e5b41af50c2a16a6f4d68
Jhamil Taborga <jhamiltaborga@gmail.com>
2025-02-04 19:54:59 -0400

    Creamos una carpeta donde vivirá la url de la API, cambié el método para usar la API de .then a async y await

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,26 +1,37 @@
 import { useState, useEffect } from 'react'
 import Layout from '../../components/Layout'
 import Card from '../../components/Card'
+import { apiUrl } from '../../Api'
 
 function Home() {
   const [items, setItems] = useState(null)
 
-  useEffect(() => {
-    fetch('https://api.escuelajs.co/api/v1/products')
-      .then(response => response.json())
-      .then(data => setItems(data))
+  useEffect( () => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`)
+        const data = await response.json()
+        setItems(data)
+      } catch (error) {
+        console.error(`Oh no, ocurrió un error: ${error}`)  
+      }
+    }
+    fetchData()
   }, [])
 
     return (
       <Layout> 
         Home
-        <div className='grid grid-cols-4 gap-4 w-full max-w-screen-lg'>
+        <section className='grid grid-cols-4 gap-4 w-full max-w-screen-lg'>
         {
           items?.map((item) => (
-            <Card key={item.id} data={item} />
+            <Card 
+              key={item.id} 
+              data={item} 
+            />
           ))
         }
-        </div>
+        </section>
       </Layout>
     )
   }
