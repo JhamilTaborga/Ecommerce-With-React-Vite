8de0daf4ced4bea4412f649241e0ed9fc4331795
Jhamil Taborga <jhamiltaborga@gmail.com>
2025-02-12 17:59:42 -0400

    Creé un estado para mostrar el ProductDetail cuando se le da un click a alguna card

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,18 +3,26 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({ children }) => {
+    // Shopping Cart · Increment quantity
     const [count, setCount] = useState(0)
+
+    // Product Detail · Open/Close
     const [isProductDetailOpen, setIsproductDetailOpen] = useState(false)
     const openProductDetail = () => setIsproductDetailOpen(true)
     const closeProductDetail = () => setIsproductDetailOpen(false)
     
+    // Product Detail · Show product
+    const [productToShow, setProductToShow] = useState({})
+
     return (
         <ShoppingCartContext.Provider value={{
             count,
             setCount,
             isProductDetailOpen,
             openProductDetail,
-            closeProductDetail
+            closeProductDetail,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -5,12 +5,13 @@ import './styles.css'
 
 const ProductDetail = () => {
     const context = useContext(ShoppingCartContext)
+    console.log('PRODUCT TO SHOW: ', context.productToShow)
 
     return (
         <aside 
             className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}
         >
-            <div className='flex justify-between itmes-center p-6'>
+            <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
                 <button>
                     <XMarkIcon 
@@ -19,6 +20,18 @@ const ProductDetail = () => {
                     />
                 </button>
             </div>
+            <figure className='px-6 flex flex-col overflow-y-auto'>
+                <img 
+                    className='w-full rounded-lg'
+                    src={context.productToShow.images?.[0]} 
+                    alt={context.productToShow.title} 
+                />
+                <p className='flex flex-col p-6'>
+                    <span className='font-medium text-2xl'>${context.productToShow.price}</span>
+                    <span className='font-medium text-md'>${context.productToShow.title}</span>
+                    <span className='font-medium text-sm'>${context.productToShow.description}</span>
+                </p>
+            </figure>
         </aside>
     )
 }
