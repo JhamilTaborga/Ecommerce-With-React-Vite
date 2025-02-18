49eb97e070af9a323cf1061b8edd63ee8f8cdc85
Jhamil Taborga <jhamiltaborga@gmail.com>
2025-02-18 18:57:32 -0400

    Agregamos la funcionalidad para que no se dupliquen los elementos y también para que cambie de boton cuando ya se selecciona algo

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Card = (data) => {
@@ -18,7 +18,31 @@ const Card = (data) => {
         context.openCheckoutSideMenu()
         context.closeProductDetail()
     }
-    
+
+    const renderIcon = (id) => {
+        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
+
+        if (isInCart) {
+            return (
+                <button 
+                    className='absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1'
+                    onClick={(event) => event.stopPropagation()}
+                >
+                    <CheckIcon className='size-6 text-white' />
+                </button>
+            )
+        } else {
+            return (
+            <button 
+                className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
+                onClick={(event) => addProductsToCart(event, data.data)}
+            >
+                <PlusIcon className='size-6 text-black' />
+            </button>
+            )
+        }
+    }
+
     return (
         <article 
             className='bg-white cursor-pointer w-56 h-60 rounded-lg shadow-lg'
@@ -27,12 +51,7 @@ const Card = (data) => {
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
-                <button 
-                    className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-                    onClick={(event) => addProductsToCart(event, data.data) }
-                >
-                    <PlusIcon className='size-6 text-black' />
-                </button>
+                {renderIcon(data.data.id)}
             </figure>
             <p className='flex justify-between items-center px-3'>
                 <span className='text-sm font-light'>{data.data.title}</span>
