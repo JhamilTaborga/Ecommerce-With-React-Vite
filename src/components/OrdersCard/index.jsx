c4c3ea09e5f66a48d9d043a8b28eeabee63c044c
Jhamil Taborga <jhamiltaborga@gmail.com>
2025-03-21 19:40:35 -0400

    mejoré los estilos de la lista de ordenes/OrdersCard

--- src/components/OrdersCard/index.jsx
+++ src/components/OrdersCard/index.jsx
@@ -1,15 +1,24 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { CalendarDaysIcon, ShoppingBagIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = props => {
     const { totalPrice, totalProducts } = props
 
     return (
-        <div className='flex justify-center items-center w-60 h-24 mb-6 border border-black border-opacity-45 rounded-xl shadow-lg'>
-            <p className='flex flex-col text-left justify-between'>
-                <span>Date: 3.17.25</span>
-                <span>Products: {totalProducts}</span>
-                <span>Price: {totalPrice}</span>
-            </p>
+        <div className='flex justify-between items-center w-80 h-20 px-5 mb-4 border border-black border-opacity-70 rounded-lg shadow-lg'>
+            <div>
+                <span className='flex items-center'>
+                    <CalendarDaysIcon className='size-4 text-black mr-3' />
+                    <p className='font-light'>3.17.25</p>
+                </span>
+                <span className='flex items-center'>
+                    <ShoppingBagIcon className='size-4 text-black mr-3' />
+                    <p className='font-light'>{totalProducts}  articles</p>
+                </span>
+            </div>
+            <div className='flex items-center gap-2'>
+                <p className='font-medium text-2xl'>${totalPrice}</p>
+                <ChevronRightIcon className='size-5 text-black'/>
+            </div>
         </div>
     )
 }
